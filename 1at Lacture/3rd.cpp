e71fd96089827f83dc172d7651a84a72368932d3
ujjwalkumardas <ujjwalbabu7488@gmail.com>
2023-12-24 17:37:29 +0530

    upgrad

--- 1at Lacture/3rd.cpp
+++ 1at Lacture/3rd.cpp
@@ -1,1 +1,9 @@
- 
+ /*array in changing*/
+ #include<iostream>
+ using namespace std;
+ int main(){
+    int arr[5]={1,2,3,4,5};
+    for(int i=0;i<5;i++){
+        cout<<arr[i];
+    }
+ }
--- 1at Lacture/4th.cpp
+++ 1at Lacture/4th.cpp
@@ -1,2 +1,17 @@
 /*updating in function then also update in main function */
-
+#include<iostream>
+using namespace std;
+void fun(int arr[],int size){
+    for(int i=0;i<size;i++){
+        arr[i]=arr[i]+1;
+        cout<<arr[i];
+    }
+}
+int main(){
+    int array[9]={4,5,8,7,3,9,2,5,8};
+    fun(array,9);
+    cout<<endl;
+    for(int i=0;i<9;i++){
+        cout<<array[i];
+    }
+}
